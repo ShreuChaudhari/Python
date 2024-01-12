4461423b97a60cfb366f3ace4d99fbe49b68c0ce
ShreuChaudhari <140592211+ShreuChaudhari@users.noreply.github.com>
2024-01-12 12:58:36 +0530

    Add files via upload

---
+++ EXP 1/Exp1.py
@@ -0,0 +1,30 @@
+num1= int(input ("Enter the value of first number:"))
+num2= int(input ("Enter the value of second number:"))
+
+#Addition
+sum = num1+num2
+print(sum)
+
+#subtraction
+Diff= num1-num2
+print(Diff)
+
+#Multiplication
+Multi= num1*num2
+print(Multi)
+
+#Division
+Div= num1/num2
+print(Div)
+
+#Modulus
+Mod = num1%num2
+print(Mod)
+
+#Exponential
+Exp=num1**num2
+print(Exp)
+
+#Floor Division
+FlDiv=num1//num2
+print(FlDiv)
